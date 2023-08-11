f841b460673e3cbe35ad5b05cf368e9647235489
WALEON-X <kingblayz100@gmail.com>
2023-08-10 20:46:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that print a line using printf
+ * Return: 0(Sucesss)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
+}
