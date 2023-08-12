c6f2e89b15fd4dfe2e33406d8164fe9061dc96b1
WALEON-X <kingblayz100@gmail.com>
2023-08-11 21:48:12 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Advance)
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 'a';
+	b = 'A';
+	while
+		(a <= 'z') {
+			putchar(a);
+			c++;
+		}
+	while
+		(b <= 'Z') {
+			putchar (b);
+		}
+	putchar('\n');
+	return (0);
