565d1acde0bea65618a88e1d0481e6e9e960a1c1
WALEON-X <kingblayz100@gmail.com>
2023-08-14 19:45:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Advance)
+ */
+
+int main(void)
+{
+	int w;
+
+	for (w = 0; w < 19; w++)
+		printf("%d", w);
+	putchar('\n');
+	return (0);
+}
