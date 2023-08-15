a525ce9fefbad334c9a02b75642395522212e48f
WALEON-X <kingblayz100@gmail.com>
2023-08-14 20:20:18 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Advance)
+ */
+
+int main(void)
+{
+	char w;
+
+	int x;
+
+	w = 'a';
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	while
+		(w <= 'f') {
+			putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
