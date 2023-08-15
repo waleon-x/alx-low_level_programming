b74e3a0cbd6298e7f4705b52e3af90119ae9153c
WALEON-X <kingblayz100@gmail.com>
2023-08-14 19:36:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main = Entry point
+ * Return: Always 0 (Advance)
+ */
+
+int main(void)
+{
+	char w;
+
+	w = 'a';
+	while
+		(w <= 'z') {
+			if ((w != 'q' && w != 'e') && w <= 'z')
+				putchar(w);
+			w++;
+		}
+	putchar('\n');
+	return (0);
+}
