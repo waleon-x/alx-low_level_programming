8a19abff6310f1d7311834165e1920a4d4937347
WALEON-X <kingblayz100@gmail.com>
2023-08-14 20:06:37 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Advance)
+ */
+
+int main(void)
+{
+	char w;
+
+
+	w = 'z';
+	while
+		(w >= 'a') {
+			putchar(w);
+			w--;
+		}
+	putchar ('\n');
+	return (0);
+}
+
