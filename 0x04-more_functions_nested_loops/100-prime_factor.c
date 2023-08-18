c23415bffd15fed9ea3e0c34b85fc203d9c00883
WALEON-X <kingblayz100@gmail.com>
2023-08-18 09:26:52 -0700

    11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print prime factor
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	long x, maxf;
+       long number = 612852475143;
+       double square = sqrt(number);
+
+       for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+       printf("%ld\n", maxf);
+       return (0);
+}
