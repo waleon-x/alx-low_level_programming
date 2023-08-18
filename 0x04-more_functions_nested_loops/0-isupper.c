164d9f0a8a53b604c79d3c4a2028715b075c3753
WALEON-X <kingblayz100@gmail.com>
2023-08-17 20:32:36 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check for uppercase
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
