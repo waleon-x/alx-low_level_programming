a93729081f710cc6707c481310a39adcf363b082
WALEON-X <kingblayz100@gmail.com>
2023-08-21 18:25:22 -0700

    0.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - function that takes a pointer to an int as parameter
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+C programming
+0x05. C - Pointers, arrays and strings
