860ba71f24ee2fec3dfa20663145f2f83864ed70
WALEON-X <kingblayz100@gmail.com>
2023-08-29 10:21:07 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int u = 0;
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				u++;
+				break;
+			}
+			else if (accept[x + 1] == '\0')
+				return (u);
+		}
+		s++;
+	}
+	return (u);
+}
