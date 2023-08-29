0be07615a5c3d2e408c2ce627310c096c1b5ad4b
WALEON-X <kingblayz100@gmail.com>
2023-08-29 10:16:48 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Advance)
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	for (; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (&s[x]);
+	}
+	return (0);
+}
