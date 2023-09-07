9dcb5e54bd8273ff198659acd5f75059d4eac619
WALEON-X <kingblayz100@gmail.com>
2023-09-06 21:57:58 -0700

    task o

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy - function that copies memory 
+ *@dest: memory where it is stored
+ *@src: memory where it is copied
+ *@n: number of bytes
+ *Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        	int x = 0;
+        	int y = n;
+ 
+        	for (; x < y; x++)
+        	{
+                    	dest[x] = src[x];
+                    	n--;
+        	}
+        	return (dest);
+}
