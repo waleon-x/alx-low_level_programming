ba481783a6aa9d07e69ef202feed4023ae020f8a
WALEON-X <kingblayz100@gmail.com>
2023-09-11 19:21:13 -0700

    task 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,10 +4,10 @@
 
 	/**
 	 * struct dog - a dog's  information
-	 * @name: First input
-	 * @age: Second input
-	 * @owner: Third input
-	 * Description: Longer description
+	 * @name: dog's name
+	 * @age: dog's age
+	 * @owner: dog's owner
+	 * Description: Longer descriptioin
 	 */
 	struct dog
 	{
@@ -16,15 +16,9 @@
 		char *owner;
 	};
 
-
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
+	typedef struct dog dog_t;
 	dog_t *new_dog(char *name, float age, char *owner);
 	void free_dog(dog_t *d);
 	char *_strcpy(char *dest, char *src);
